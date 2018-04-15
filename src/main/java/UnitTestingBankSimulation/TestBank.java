e03a0f2e71229bfbeb6134d3cc9931e49777a462
Ihor Panchenko <ipanchenko91@gmail.com>
2018-04-15 18:30:42 -0400

    Create TestBank.java

---
+++ src/main/java/UnitTestingBankSimulation/TestBank.java
@@ -0,0 +1,17 @@
+package UnitTestingBankSimulation;
+
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.SortedSet;
+import org.junit.*;
+
+public class TestBank {
+
+	Bank testBank;
+	final String bankName = "Test Name";
+	final String lastName = "Panchenko";
+	final String firstName = "Ihor";
+	String testCustomer_1, testCustomer_2;
+	
+	
+}
