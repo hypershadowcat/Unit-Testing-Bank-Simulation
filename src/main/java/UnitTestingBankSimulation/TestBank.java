bcbdf9e1eaeb56e314076a820d6ab57ed7b8837d
Ihor Panchenko <ipanchenko91@gmail.com>
2018-04-15 18:58:41 -0400

    @Before and bank constructor test.

--- src/main/java/UnitTestingBankSimulation/TestBank.java
+++ src/main/java/UnitTestingBankSimulation/TestBank.java
@@ -13,5 +13,26 @@ public class TestBank {
 	final String firstName = "Ihor";
 	String testCustomer_1, testCustomer_2;
 	
-	
+	// Creates similar objects for tests which execute before the tests are run
+	@Before
+	public void init() {
+		// Creates Bank object
+		testBank = new Bank(bankName);
+		
+		// Adds two customers
+		testCustomer_1 = testBank.addCustomer(lastName, firstName);
+		testCustomer_2 = testBank.addCustomer(lastName, firstName);
+	}
+
+	// Bank constructor test
+	@Test
+	public void testBank() {
+		// Asserts that the object isn't null. If it is an AssertionError is thrown
+		assertNotNull(testBank); // when bank object exists -- true
+		
+		// Asserts that two objects are equal
+		assertEquals(testBank.getNAME(), bankName); // when bank name is correct -- true
+	}
+
+
 }
