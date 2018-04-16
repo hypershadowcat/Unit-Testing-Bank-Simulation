128ef2871f0bf2d11061b7dbfede8ebb631fa921
Ihor Panchenko <ipanchenko91@gmail.com>
2018-04-16 13:02:28 -0400

    getAllAccounts, addCustomer, getCustomer, getAllCustomers tests.

--- src/main/java/UnitTestingBankSimulation/TestBank.java
+++ src/main/java/UnitTestingBankSimulation/TestBank.java
@@ -12,6 +12,8 @@ public class TestBank {
 	final String lastName = "Panchenko";
 	final String firstName = "Ihor";
 	String testCustomer_1, testCustomer_2;
+	// Creates the customer list
+	List<Customer> customerList = testBank.getCustomer(lastName, firstName);
 	
 	// Creates similar objects for tests which execute before the tests are run
 	@Before
@@ -34,5 +36,49 @@ public class TestBank {
 		assertEquals(testBank.getNAME(), bankName); // when bank name is correct -- true
 	}
 
+	// Method getAllAccounts test
+	@Test
+	public void testGetAllAccounts() {
+		final double accountBalance = 741.95;
+		final String description = "Test description";
+		// Creates the account in the set
+		Account testAccount = new SavingsAccount(testBank.getCustomer(testCustomer_1),
+				accountBalance, description);
+		
+		// Creates the accounts set
+		SortedSet<Account> accountsSet = testBank.getAllAccounts();
+		assertNotNull(accountsSet); // when Set exists -- true
+		
+		// Asserts that the first (lowest) element currently in this set and customer are equal
+		assertEquals(accountsSet.first().getCustomerId(), testCustomer_1); // when 1st customerID is correct -- true
+		assertEquals(accountsSet.first().getAccountDescription(), description); // when 1st description is correct -- true
+		assertTrue(accountsSet.first().getBalance() == accountBalance); // when 1st balance is correct -- true
+	}
+	
+	// Method addCustomer test
+	@Test
+	public void testAddCustomer() {
+		assertNotNull(testCustomer_1); // when testCustomer_1 returned by the method -- true
+	}
 
+	// Method getCustomer test
+	@Test
+	public void testGetCustomer() {
+		Customer customer = testBank.getCustomer(testCustomer_1);
+		assertNotNull(customer); // when customer object exists -- true
+		assertEquals(customer.getLastName(), lastName); // when last name is correct -- true
+		assertEquals(customer.getFirstName(), firstName); // when last name is correct -- true
+	}
+	
+	// Method getAllCustomers test
+	@Test
+	public void testGetAllCustomers() {
+		// Creates the customer set
+		SortedSet<Customer> customerSortedSet = testBank.getAllCustomers();
+		assertNotNull(customerSortedSet);
+		assertEquals(customerSortedSet.first().getLastName(), lastName);
+		assertEquals(customerSortedSet.first().getFirstName(), firstName);
+	}
+
+	
 }
