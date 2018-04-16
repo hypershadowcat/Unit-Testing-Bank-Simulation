308c6c697069a33e9a96939e630fe2cba7f43599
Craig Anderson <canderson93@hawkmail.hccfl.edu>
2018-04-16 19:57:14 -0400

    Refactored Savings Account

---
+++ src/main/java/UnitTestingBankSimulation/TestSavingsAccount.java
@@ -0,0 +1,14 @@
+package UnitTestingBankSimulation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSavingsAccount {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
