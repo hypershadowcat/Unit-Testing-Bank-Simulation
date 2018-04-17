4d7d7265404c0c09e3312602f402daa076093104
Craig Anderson <canderson93@hawkmail.hccfl.edu>
2018-04-16 21:35:28 -0400

    Added TestSavingsAccount with test methods.

--- src/main/java/UnitTestingBankSimulation/TestSavingsAccount.java
+++ src/main/java/UnitTestingBankSimulation/TestSavingsAccount.java
@@ -2,13 +2,47 @@ package UnitTestingBankSimulation;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSavingsAccount {
-
+	
+	Bank testBank;
+	Customer testCustomer;
+	SavingsAccount testSavings;
+	final String bankName = "Test Name";
+	final String lastName = "Anderson";
+	final String firstName = "Craig";
+	final double initialBalance = 800.23;
+	final double rate = 2.1;
+	final String description = "Test Description";
+	
+	
+	@Before
+	public void init() {
+		// Creates Bank object
+		testBank = new Bank(bankName);
+		// Creates Customer object
+		testCustomer = new Customer(testBank, lastName, firstName);
+		// Creates Saving Account object
+		testSavings = new SavingsAccount(testCustomer, initialBalance, description);
+		
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSavingsAccount() {
+		//Tests 
+		assertNotNull(testSavings);
+		
+		assertEquals(testSavings.getAccountDescription(), description);
+		
+		assertNotNull(initialBalance);
+	}
+	@Test
+	public void testAddInterest() {
+		testSavings.addInterestTransaction(rate);
+		assertNotNull(rate);
 	}
 
 }
