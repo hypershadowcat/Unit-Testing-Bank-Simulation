104f626755216ecb59f2ddbb7cc3d455c6f85f06
Craig Anderson <canderson93@hawkmail.hccfl.edu>
2018-04-16 22:01:29 -0400

    Saved work

---
+++ src/main/java/UnitTestingBankSimulation/TestTransaction.java
@@ -0,0 +1,41 @@
+package UnitTestingBankSimulation;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTransaction {
+	
+	Bank testBank;
+	Customer testCustomer;
+	SavingsAccount testSavings;
+	Transaction testTransaction;
+	final String bankName = "Test Name";
+	final String lastName = "Anderson";
+	final String firstName = "Craig";
+	final double initialBalance = 800.23;
+	final double rate = 2.1;
+	final String description = "Test Description";
+	private final int id;
+    private final LocalDateTime timestamp;
+    private final TransactionType type;
+    private final double amount;
+    private final String description;
+	
+	@Before
+	public void init() {
+		// Creates Bank object
+		testBank = new Bank(bankName);
+		// Creates Customer object
+		testCustomer = new Customer(testBank, lastName, firstName);
+		// Creates Saving Account object
+		testSavings = new SavingsAccount(testCustomer, initialBalance, description);
+		// Creates Transaction object
+		testTransaction = new Transaction();
+		
+	}
+
+}
