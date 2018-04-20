964195800bcf50629b91e546017ede986a89e060
originalxaque <originalxaque@users.noreply.github.com>
2018-04-20 14:15:46 -0400

    Created @Before init and first test to ensure object is created.

--- src/main/java/UnitTestingBankSimulation/TestCustomer.java
+++ src/main/java/UnitTestingBankSimulation/TestCustomer.java
@@ -1,14 +1,31 @@
 package UnitTestingBankSimulation;
 
 import static org.junit.Assert.*;
+import org.junit.*;
 
 // Written by Zachary Willis, Team Spirit Bomb, 2018
 
 public class TestCustomer {
 	
     Customer testCustomer;
-    final String testCustomerID = "001";
+    final Bank testBank = new Bank("testBank");
     final String testLastName = "Bobob";
     final String testFirstName = "Jojo";
+    
+    // Set up clean slate for all tests
+    @Before
+    public void init() {
+    	// Create customer object
+    	testCustomer = new Customer(testBank, testLastName, testFirstName);
+    }
+    
+    @Test
+    public void testCustomerCreated() {
+    	assertNotNull(testCustomer); // Ensures the customer object exists.
+    	
+    	assertEquals(testCustomer.getBank(), testBank); // Ensures the bank name of customer object is correct bank for test.
+    }
+    
+    
     
 }
