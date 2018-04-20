581ed739ddd3ae2b30b7177e4192bf0a76286cfa
originalxaque <originalxaque@users.noreply.github.com>
2018-04-20 13:50:30 -0400

    Set up initial TestCustomer file for rollback.

---
+++ src/main/java/UnitTestingBankSimulation/TestCustomer.java
@@ -0,0 +1,14 @@
+package UnitTestingBankSimulation;
+
+import static org.junit.Assert.*;
+
+// Written by Zachary Willis, Team Spirit Bomb, 2018
+
+public class TestCustomer {
+	
+    Customer testCustomer;
+    final String testCustomerID = "001";
+    final String testLastName = "Bobob";
+    final String testFirstName = "Jojo";
+    
+}
